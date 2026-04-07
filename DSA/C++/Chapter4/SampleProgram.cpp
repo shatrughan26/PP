86f4caebdc9aab869da959897bfed3f860243ff9
Deepak kumar sharma <deepak4996sharma@gmail.com>
2026-04-07 10:40:18 +0530

    start from page 84

---
+++ DSA/C++/Chapter4/SampleProgram.cpp
@@ -0,0 +1,12 @@
+//page numebr 83
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << uppercase << "octal \t decimal  \t hexadecimal\n " << oct << number << "      \t" << dec << number << "       \t " << hex << number << endl;
+    
+    return 0;
+}
